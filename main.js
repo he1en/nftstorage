7c6d1032ebe5d8e30bb5953e6737e2a009288256
Helen Hastings <helenbhastings@gmail.com>
2022-06-10 12:52:48 -0700

    fetch token data, parse ipfs

--- main.js
+++ main.js
@@ -37,8 +37,31 @@ const ABI = [
     }
 ];
 
-const othersideContractAddress = '0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258';
-const contract = new web3.eth.Contract(ABI, othersideContractAddress);
-const tokenID = 65995;
-contract.methods.name().call((err, result) => { console.log(result) });
-contract.methods.tokenURI(tokenID).call((err, result) => { console.log(result) });
+// todo make these user input
+const tokenID = 4277;
+const contractAddress = '0xbCe3781ae7Ca1a5e050Bd9C4c77369867eBc307e'; //'0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258'
+
+function getURL(uri) {
+    if (!uri.startsWith('ipfs://')) {
+        return uri;
+    }
+    return `https://cloudflare-ipfs.com/ipfs/${uri.slice(7)}`;
+}
+
+async function getToken() {
+    const contract = new web3.eth.Contract(ABI, contractAddress);
+
+    const name = await contract.methods.name().call();
+    console.log(`NFT Name: ${name}`);
+
+    const tokenURI = await contract.methods.tokenURI(tokenID).call();
+    const tokenURL = getURL(tokenURI);
+    const response = await fetch(tokenURL);
+    const tokenData = await response.json();
+    console.log(tokenData);
+    const imageURL = getURL(tokenData.image);
+    console.log(imageURL);
+    //const image = fetch()
+}
+
+getToken()
