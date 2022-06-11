10b5cd6c3c8bf0e4d7bf8f53d1a832f58bf7a427
Helen Hastings <helenbhastings@gmail.com>
2022-06-11 10:19:48 -0700

    checks if there is an owner owned base uri

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const web3 = new Web3(ethProvider);
 
 const ABI = [
     {
-        "inputs": [],
         "name": "name",
+        "inputs": [],
         "outputs": [
             {
                 "internalType": "string",
@@ -13,10 +13,11 @@ const ABI = [
                 "type": "string"
             }
         ],
+        "type": "function",
         "stateMutability": "view",
-        "type": "function"
     },
     {
+        "name": "tokenURI",
         "inputs": [
             {
                 "internalType": "uint256",
@@ -24,7 +25,6 @@ const ABI = [
                 "type": "uint256"
             }
         ],
-        "name": "tokenURI",
         "outputs": [
             {
                 "internalType": "string",
@@ -32,14 +32,28 @@ const ABI = [
                 "type": "string"
             }
         ],
+        "type": "function",
         "stateMutability": "view",
-        "type": "function"
+    },
+    {
+        "name": "setBaseURI",
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "uri",
+                "type": "string"
+            }
+        ],
+        "outputs": [],
+        "type": "function",
+        "stateMutability":"nonpayable"
     }
 ];
 
 // todo make these user input
 const tokenID = 4277;
-const contractAddress = '0xbCe3781ae7Ca1a5e050Bd9C4c77369867eBc307e'; //'0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258'
+//const contractAddress = '0xbCe3781ae7Ca1a5e050Bd9C4c77369867eBc307e'; // globin
+const contractAddress = '0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258' // otherside
 
 function getURL(uri) {
     if (!uri.startsWith('ipfs://')) {
@@ -49,10 +63,21 @@ function getURL(uri) {
 }
 
 async function getToken() {
+    web3.eth.handleRevert = true;
     const contract = new web3.eth.Contract(ABI, contractAddress);
 
-    const name = await contract.methods.name().call();
+
+    const name = await contract.methods.name().call()
     console.log(`NFT Name: ${name}`);
+
+
+    contract.methods.setBaseURI('new_uri').estimateGas()
+        .then(response => console.log('this cant happen'))
+        .catch(err => {
+            if (err.message.includes('Only operator can call this method')) {
+                console.log('The owner of the contract for this NFT can change the above link at any time.')
+            }
+        });
 
     const tokenURI = await contract.methods.tokenURI(tokenID).call();
     const tokenURL = getURL(tokenURI);
@@ -62,6 +87,8 @@ async function getToken() {
     const imageURL = getURL(tokenData.image);
     console.log(imageURL);
     //const image = fetch()
+
+    console.log(`You can read the source code for this NFT at https://etherscan.io/address/${contractAddress}#code`)
 }
 
 getToken()
