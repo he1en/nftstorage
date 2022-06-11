9ddb7e88fb35fb54243472daef8b9e3ba388478e
Helen Hastings <helenbhastings@gmail.com>
2022-06-11 13:40:23 -0700

    basic static website working

--- main.js
+++ main.js
@@ -1,4 +1,41 @@
-const Web3 = require('web3');
+// ------------- DOM access ------------------
+async function readNFT() {
+    var input = document.getElementById("main-input").value;
+    validateInput(input);
+    const {contractAddress, tokenID} = parseMarketplaceLink(input);
+    const nft = await getNFTInfo(contractAddress, tokenID);
+
+    document.getElementById("nft-name").innerHTML = `Your ${nft.name} NFT`;
+    document.getElementById("nft-location").innerHTML = `Its image is located at ${nft.imageURI}, not on the blockchain`;
+    document.getElementById("nft-data").src = queryableURL(nft.tokenURI);
+    if (nft.knownChangeable) {
+        document.getElementById("nft-changeable").innerHTML = `Also, according to the source code for this NFT, someone can change the above link at any time.\n`;
+    } else {
+        document.getElementById("nft-changeable").hidden = false;
+    }
+    var sourceLink = document.getElementById("nft-source");
+    sourceLink.innerHTML = "You can read the source code for this NFT here."
+    sourceLink.href = `https://etherscan.io/address/${contractAddress}#code`;
+
+    var image = document.getElementById("nft-image");
+    image.src = queryableURL(nft.imageURI);
+    image.hidden = false;
+}
+
+var input = document.getElementById("main-input");
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        readNFT();
+    }
+});
+
+var button = document.getElementById("main-button");
+button.onclick = readNFT;
+
+
+
+// ----- Just Javascript ---------
+
 const ethProvider = 'https://cloudflare-eth.com';
 const web3 = new Web3(ethProvider);
 
@@ -50,19 +87,46 @@ const ABI = [
     }
 ];
 
-// todo make these user input
-const tokenID = 4277;
-//const contractAddress = '0xbCe3781ae7Ca1a5e050Bd9C4c77369867eBc307e'; // globin
-const contractAddress = '0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258' // otherside
 
-function getURL(uri) {
+function validateInput(input) {
+    if (input.startsWith('https://opensea.io/assets/ethereum') ||
+        input.startsWith('https://looksrare.org/collections')) {
+            return true;
+    }
+    return false;
+}
+
+
+function parseMarketplaceLink(marketplaceLink) {
+    if (marketplaceLink.endsWith('/')) {
+        marketplaceLink = marketplaceLink.substring(0, marketplaceLink.length - 1);
+    }
+    const URLparts = marketplaceLink.split('/');
+    const tokenID = parseInt(URLparts[URLparts.length - 1]);
+    const contractAddress = URLparts[URLparts.length - 2];
+    return {tokenID, contractAddress};
+
+}
+
+
+function queryableURL(uri) {
     if (!uri.startsWith('ipfs://')) {
         return uri;
     }
-    return `https://cloudflare-ipfs.com/ipfs/${uri.slice(7)}`;
+    return `https://ipfs.io/ipfs/${uri.slice(7)}`;
 }
 
-async function getToken() {
+class NFT {
+    constructor(name, tokenURI, tokenData, imageURI, knownChangeable) {
+        this.name = name;
+        this.tokenURI = tokenURI;
+        this.tokenData = tokenData;
+        this.imageURI = imageURI;
+        this.knownChangeable = knownChangeable;
+    }
+}
+
+async function getNFTInfo(contractAddress, tokenID) {
     web3.eth.handleRevert = true;
     const contract = new web3.eth.Contract(ABI, contractAddress);
 
@@ -71,24 +135,22 @@ async function getToken() {
     console.log(`NFT Name: ${name}`);
 
 
-    contract.methods.setBaseURI('new_uri').estimateGas()
-        .then(response => console.log('this cant happen'))
+    var isChangeable = false;
+    await contract.methods.setBaseURI('new_uri').estimateGas()
+        .then(response => {console.log(response); response.json().then(data=>console.log(data));})
         .catch(err => {
+            console.log(err);
             if (err.message.includes('Only operator can call this method')) {
-                console.log('The owner of the contract for this NFT can change the above link at any time.')
+                isChangeable = true;
             }
         });
 
     const tokenURI = await contract.methods.tokenURI(tokenID).call();
-    const tokenURL = getURL(tokenURI);
-    const response = await fetch(tokenURL);
+    const response = await fetch(queryableURL(tokenURI));
     const tokenData = await response.json();
-    console.log(tokenData);
-    const imageURL = getURL(tokenData.image);
-    console.log(imageURL);
-    //const image = fetch()
-
-    console.log(`You can read the source code for this NFT at https://etherscan.io/address/${contractAddress}#code`)
+    const nft = new NFT(name, tokenURI, tokenData, tokenData.image, isChangeable);
+    console.log(nft);
+    return nft;
 }
 
-getToken()
+
