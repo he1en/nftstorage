213ad7e6c45a49ab071ceec379559b0d74f9cd65
Helen Hastings <helenbhastings@gmail.com>
2022-06-15 21:16:36 -0700

    support base64 encoded on chain content

--- main.js
+++ main.js
@@ -2,15 +2,13 @@
 async function readNFT() {
 
     // reset in case of errors
-    document.getElementById("main-output").hidden = true;
+    resetAllHidden();
 
     // validate input
     var input = document.getElementById("main-input").value;
     if (!validateInput(input)) {
         document.getElementById("error-output").hidden = false;
         return;
-    } else {
-        document.getElementById("error-output").hidden = true;
     }
 
     // query nft data
@@ -22,10 +20,36 @@ async function readNFT() {
 
     // build dom
     document.getElementById("main-output").hidden = false;
-    buildOnChainSection(nft, tokenID);
-    buildOffChainSection(nft);
+    buildHeader(nft, tokenID);
+    var traitsID;
+    if (nft.onChain) {
+        traitsID = "nft-on-chain-traits";
+    } else {
+        buildOnChainSection(nft, tokenID);
+        buildOffChainSection(nft);
+        traitsID = "nft-off-chain-traits";
+    }
+    document.getElementById(traitsID).innerHTML = formatTraits(nft.tokenData);
+    document.getElementById(traitsID).hidden = false;
     buildExplanatorySection(nft, tokenID, contractAddress);
     document.getElementById("footer").hidden = false;
+}
+
+function resetAllHidden() {
+    const elemIDs = [
+        "main-output",
+        "error-output",
+        "off-chain-nft-location",
+        "nft-on-chain-traits",
+        "off-chain",
+        "thats-it",
+        "footer",
+        "cryptopunks-output",
+        "nft-source"
+    ];
+    for (var elemId of elemIDs) {
+        document.getElementById(elemId).hidden = true;
+    }
 }
 
 function handleCryptoPunks(contractAddress) {
@@ -39,10 +63,8 @@ function handleCryptoPunks(contractAddress) {
         buildSource(contractAddress, "CryptoPunks");
         document.getElementById("footer").hidden = false;
         return true;
-    } else {
-        document.getElementById("cryptopunks-output").hidden = true;
-        return false;
     }
+    return false;
 }
 
 
@@ -51,16 +73,32 @@ function URItoLink(uri) {
     return `<a class="token-uri" href=${url} target="_blank"><b>${uri}</b></a>`
 }
 
+function buildHeader(nft, tokenID) {
+    /* token name and image at top */
+    document.getElementById("nft-name").innerHTML = `${nft.name} #${tokenID}`;
+
+    var imgElem = document.getElementById("main-image");
+    var imgContent;
+    if (nft.imageURI.startsWith("data:image/svg+xml;base64,")) {
+        const content = atob(nft.imageURI.slice("data:image/svg+xml;base64,".length))
+        imgElem.style.height = "100%";
+        imgElem.style.width = "100%";
+        imgContent = content;
+    } else {
+        imgContent = `<img class="nft-image" src=${queryableURL(nft.imageURI, true)}></img>`;
+    }
+    imgElem.innerHTML = imgContent;
+
+}
 
 function buildOnChainSection(nft, tokenID) {
-    document.getElementById("nft-name").innerHTML = `${nft.name} #${tokenID}`;
-    document.getElementById("main-image").src = queryableURL(nft.imageURI, true);
-
     const ownerHTML = `<b>${nft.name} #${tokenID}</b> is owned by the account <b>${nft.ownerAddr}</b>.`
     document.getElementById("nft-owner").innerHTML = ownerHTML;
 
     const locHTML = `The URI for <b>${nft.name} #${tokenID}</b> is ${URItoLink(nft.tokenURI)}.`;
-    document.getElementById("nft-location-upper").innerHTML = locHTML;
+    document.getElementById("off-chain-nft-location").innerHTML = locHTML;
+    document.getElementById("off-chain-nft-location").hidden = false;
+    document.getElementById("thats-it").hidden = false;
 }
 
 
@@ -69,29 +107,34 @@ function formatTraits(tokenDataJSON) {
     Turn the NFT traits metadata json into a table.
     Force the image element to the bottom for emphasis.
     */
-
     const {image, ...rest} = tokenDataJSON;
-    var result = ""
+    var tableHTML = "";
     for(var key of Object.keys(rest)) {
         const content = rest[key];
-        result += `<tr><th>${key}</th><td>${JSON.stringify(content, undefined, 2).replace(/\"/g, "")}</td></tr>`
+        tableHTML += `<tr><th>${key}</th><td>${JSON.stringify(content, undefined, 2).replace(/\"/g, "")}</td></tr>`;
     }
-    result += `<tr><th>image</th><td>${URItoLink(image)}</td></tr>`
-    return `<table><tbody>${result}</tbody></table>`
+    var imgContent;
+    if (image.startsWith("data:image/svg+xml;base64,")) {
+        imgContent = image.slice(0, "data:image/svg+xml;base64,".length + 200) + "......";
+    } else{
+        imgContent = URItoLink(image);
+    }
+    tableHTML += `<tr><th>image</th><td>${imgContent}</td></tr>`
+    return `<table><tbody>${tableHTML}</tbody></table>`
 }
 
 
 function buildOffChainSection(nft) {
+    document.getElementById("off-chain").hidden = false;
+
     const headerTxt = `What's NOT stored on the blockchain, but instead at ${URItoLink(nft.tokenURI)}:`;
     document.getElementById("off-chain-heading").innerHTML = headerTxt;
-
-    const traits = formatTraits(nft.tokenData);
-    document.getElementById("nft-traits").innerHTML = traits;
 }
 
 
 function buildSource(contractAddress, name) {
     var sourceLink = document.getElementById("nft-source");
+    sourceLink.hidden = false;
     sourceLink.innerHTML = `You can read the source code for ${name} here to see this for yourself.`;
     sourceLink.href = `https://etherscan.io/address/${contractAddress}#code`;
 }
@@ -99,21 +142,35 @@ function buildSource(contractAddress, name) {
 
 function buildExplanatorySection(nft, tokenID, contractAddress) {
 
-    var explanationText = `Why is it stored like this? It is the <a target="_blank" href="https://eips.ethereum.org/EIPS/eip-721">
-    accepted standard</a> that the representation of an NFT on the blockchain is just a pointer to another URL which
-    stores the information about the NFT, including its image.`
-    if (!(nft.tokenURI.includes('/ipfs/') || nft.tokenURI.includes('ipfs://'))) {
-        explanationText += ` Whoever owns the server behind ${URItoLink(nft.tokenURI)} can change the content
-        stored there at any time. Everything above in the red box is subject to change without the NFT owner's
-        permission.`
+    var explanationText;
+    if (nft.onChain) {
+        explanationText = `The ${nft.name} NFTs do a great job, unlike many NFTs, of keeping their data on-chain and immutable.
+        The above traits, which includes the NFT's image, are base64 encoded and stored on the blockchain. The content after
+        "image" above is truncated because it is a long base64 encoded blob that when decoded, produces the image above. `;
+        if (nft.tokenData.animation_url) {
+           explanationText += `If the NFT looks like its moving when you've seen it on a marketplace but doesn't here, it's because the
+           marketplace is using the content stored at the animation_url above, not using the image content stored on the blockchain.`;
+        }
+
+    } else {
+        explanationText = `Why is the NFT stored like this? It is the <a target="_blank" href="https://eips.ethereum.org/EIPS/eip-721">
+        accepted standard</a> that the representation of an NFT on the blockchain is just a URL which points to where the
+        information about the NFT, including its image, is stored. One reason for this is that it is incredibly expensive to store
+        content on the Ethereum blockchain.`
+        if (!(nft.tokenURI.includes('/ipfs/') || nft.tokenURI.includes('ipfs://'))) {
+            explanationText += ` But it means that whoever owns the server behind ${URItoLink(nft.tokenURI)} can change the
+            content stored there at any time. Everything above in the red box is subject to change without the NFT owner's
+            permission.`
+        }
+
+        if (nft.knownChangeable) {
+            explanationText += `<br><br>Further, according to the this NFT's contract's source code, someone can
+            call the function <b>${nft.changeFn}</b> and replace the value ${URItoLink(nft.tokenURI)}
+            on the blockchain with a different link. <b>Essentially, the owner of ${nft.name} #${tokenID}
+            only owns a link that might change at any time</b>.`
+        }
     }
 
-    if (nft.knownChangeable) {
-        explanationText += `<br><br>Further, according to the this NFT's contract's source code, someone can
-        call the function <b>${nft.changeFn}</b> and replace the value ${URItoLink(nft.tokenURI)}
-        on the blockchain with a different link. <b>Essentially, the owner of ${nft.name} #${tokenID}
-        only owns a link that might change at any time</b>.`
-    }
     document.getElementById("explanation").innerHTML = explanationText;
 
     buildSource(contractAddress, nft.name);
@@ -250,10 +307,11 @@ function queryableURL(uri, shouldProxy) {
 
 
 class NFT {
-    constructor(name, tokenURI, tokenData, imageURI, ownerAddr, knownChangeable, changeFn) {
+    constructor(name, tokenURI, tokenData, onChain, imageURI, ownerAddr, knownChangeable, changeFn) {
         this.name = name;
         this.tokenURI = tokenURI;
         this.tokenData = tokenData;
+        this.onChain = onChain;
         this.imageURI = imageURI;
         this.ownerAddr = ownerAddr;
         this.knownChangeable = knownChangeable;
@@ -272,7 +330,7 @@ async function getNFTInfo(contractAddress, tokenID) {
     await contract.methods.setBaseURI('new_uri').estimateGas()
         .then(response => {console.log(response); response.json().then(data=>console.log(data));})
         .catch(err => {
-            //console.log(err);
+            console.log(err);
             if (
                 err.message.includes('Only operator can call this method') ||
                 err.message.includes('Ownable: caller is not the owner') ||
@@ -283,12 +341,17 @@ async function getNFTInfo(contractAddress, tokenID) {
         });
 
     const tokenURI = await contract.methods.tokenURI(tokenID).call();
-    // todo handle application/json token URIs
-    const response = await fetch(queryableURL(tokenURI, true));
-    const tokenData = await response.json();
-    const nft = new NFT(name, tokenURI, tokenData, tokenData.image, ownerAddr, isChangeable, 'setBaseURI');
-    //console.log(nft);
+    var tokenData;
+    var onChain;
+    if (tokenURI.startsWith("data:application/json;base64,")) {
+        tokenData = JSON.parse(atob(tokenURI.slice("data:application/json;base64,".length)));
+        console.log(tokenData);
+        onChain = true;
+    } else {
+        const tokenResponse = await fetch(queryableURL(tokenURI, true));
+        tokenData = await tokenResponse.json();
+        onChain = false;
+    }
+    const nft = new NFT(name, tokenURI, tokenData, onChain, tokenData.image, ownerAddr, isChangeable, 'setBaseURI');
     return nft;
 }
-
-
